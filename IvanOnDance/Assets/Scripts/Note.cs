5f1fa04ca4f0e63404ab1adaec87f954ef7982c4
xMartu <xMartu@users.noreply.github.com>
2020-04-23 14:34:09 +0200

    Javichu

--- IvanOnDance/Assets/Scripts/Note.cs
+++ IvanOnDance/Assets/Scripts/Note.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
+    public KeyCode buttonKey;
+
+    bool canBePressed;
 
     void Start()
     {
-        
+        canBePressed = false;
     }
     
     void Update()
     {
-        
+        if (Input.GetKeyDown(buttonKey))
+        {
+            if (canBePressed)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = false;
+        }
     }
 }
