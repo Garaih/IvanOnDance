7b112dcc7a1fb06d32228d0b7413e7b550174397
3vilZ <55482184+3vilZ@users.noreply.github.com>
2020-04-29 12:45:01 +0200

    Idle + Anim1 + Anim2

---
+++ IvanOnDance/Assets/Scripts/Animations.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    public GameObject[] goNiggas;
+    Animator[] animNiggas;
+
+
+    void Start()
+    {
+        for (int i = 0; i < goNiggas.Length; i++)
+        {
+            animNiggas[i] = goNiggas[i].GetComponent<Animator>();
+        }
+    }
+
+    public void Anim1 (int nigga)
+    {
+        animNiggas[nigga].SetTrigger("Anim1");
+    }
+    public void Anim2 (int nigga)
+    {
+        animNiggas[nigga].SetTrigger("Anim2");
+    }
+    public void Anim3 (int nigga)
+    {
+        animNiggas[nigga].SetTrigger("Anim3");
+    }
+}
